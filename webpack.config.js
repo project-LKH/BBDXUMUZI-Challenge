b7f41d1203463504918fb7fe9278663e305a66ce
project-LKH <100766750+project-LKH@users.noreply.github.com>
2024-08-07 11:50:51 +0200

    SET UP: Basic Environment and Dependencies

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'production',
+  entry: { main: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    path: path.resolve(__dirname, "build"),
+  },
+};
